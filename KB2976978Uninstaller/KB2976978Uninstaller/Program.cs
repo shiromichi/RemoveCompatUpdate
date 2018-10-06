215f96181d4597a09d20f6d4d76da5f7026d8c94
shiromichi <ice-rain_2010.lm@live.jp>
2018-10-06 19:31:54 +0900

    Win32Securityを使用しないコードに変更 参考元: https://dobon.net/vb/dotnet/system/shutdown.html

--- KB2976978Uninstaller/KB2976978Uninstaller/Program.cs
+++ KB2976978Uninstaller/KB2976978Uninstaller/Program.cs
@@ -4,35 +4,96 @@ using System.Text;
 using System.Diagnostics;
 using System.Security.AccessControl;
 using System.Security.Principal;
+using System.Runtime.InteropServices;
 using Microsoft.Win32;
-using Microsoft.Win32.Security;
 
 namespace KB2976978Uninstaller
 {
     class Program
     {
+        // コード参考元: https://dobon.net/vb/dotnet/system/shutdown.html
+        // The MIT License (MIT)
+        // 
+        // Copyright(c) 2016 DOBON! <http://dobon.net>
+        //
+        // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetCurrentProcess();
+
+        [System.Runtime.InteropServices.DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool OpenProcessToken(IntPtr ProcessHandle,
+            uint DesiredAccess,
+            out IntPtr TokenHandle);
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        [System.Runtime.InteropServices.DllImport("advapi32.dll", SetLastError = true,
+            CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        private static extern bool LookupPrivilegeValue(string lpSystemName,
+            string lpName,
+            out long lpLuid);
+
+        [System.Runtime.InteropServices.StructLayout(
+           System.Runtime.InteropServices.LayoutKind.Sequential, Pack = 1)]
+        private struct TOKEN_PRIVILEGES
+        {
+            public int PrivilegeCount;
+            public long Luid;
+            public int Attributes;
+        }
+
+        [System.Runtime.InteropServices.DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool AdjustTokenPrivileges(IntPtr TokenHandle,
+            bool DisableAllPrivileges,
+            ref TOKEN_PRIVILEGES NewState,
+            int BufferLength,
+            IntPtr PreviousState,
+            IntPtr ReturnLength);
+
+        //所有者変更のためのセキュリティ特権を有効にする
+        public static void AdjustToken()
+        {
+            const uint TOKEN_ADJUST_PRIVILEGES = 0x20;
+            const uint TOKEN_QUERY = 0x8;
+            const int SE_PRIVILEGE_ENABLED = 0x2;
+            const string SE_TAKE_OWNERSHIP_NAME = "SeTakeOwnershipPrivilege";
+
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+                return;
+
+            IntPtr procHandle = GetCurrentProcess();
+
+            //トークンを取得する
+            IntPtr tokenHandle;
+            OpenProcessToken(procHandle,
+                TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, out tokenHandle);
+            //LUIDを取得する
+            TOKEN_PRIVILEGES tp = new TOKEN_PRIVILEGES();
+            tp.Attributes = SE_PRIVILEGE_ENABLED;
+            tp.PrivilegeCount = 1;
+            LookupPrivilegeValue(null, SE_TAKE_OWNERSHIP_NAME, out tp.Luid);
+            //特権を有効にする
+            AdjustTokenPrivileges(
+                tokenHandle, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+
+            //閉じる
+            CloseHandle(tokenHandle);
+        }
         static void Main(string[] args)
         {
-            var myProcToken = new AccessTokenProcess(Process.GetCurrentProcess().Id, TokenAccessType.TOKEN_ALL_ACCESS | TokenAccessType.TOKEN_ADJUST_PRIVILEGES);
-            try
-            {
-                myProcToken.EnablePrivilege(new TokenPrivilege(TokenPrivilege.SE_TAKE_OWNERSHIP_NAME, true));
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("管理者として実行してください。");
-                System.Environment.Exit(740);
-            }
+            AdjustToken();
 
             List<String> KB2976978List = new List<string>();
-
             using (RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Component Based Servicing\\Packages"))
             {
                 foreach (string subkeyname in registryKey.GetSubKeyNames())
                 {
                     if (subkeyname.StartsWith("Package_for_KB2976978~"))
                     {
-                        Console.WriteLine(subkeyname);
+                        Console.WriteLine("subkeyname");
                         KB2976978List.Add(subkeyname);
                         using (RegistryKey registryKey2 = registryKey.OpenSubKey(subkeyname + "\\Owners"))
                         {
